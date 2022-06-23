be7e27a7afa236dd261b9bd48229a6bba458bb2d
David Petryca <david@petryca.com>
2022-06-23 13:11:31 +0200

    latest update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
     document.querySelector('#ham').addEventListener('click', () => {
         document.querySelector('#ham').classList.toggle('open');
         document.querySelector('nav').classList.toggle('visible');
+        document.querySelector('h1').classList.toggle('hidden');
+        document.querySelector('#btn').classList.toggle('hidden');
     });
 
 
